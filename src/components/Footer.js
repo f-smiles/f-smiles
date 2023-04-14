28972f7b0c03a90df3b2ec72cc84ab689da4b1be
Jo Lee <jolee224@gmail.com>
2023-04-14 00:16:38 -0400

    fixed hover and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,40 +5,40 @@ const Footer = () => {
   <div className="bg-f7f5f2 grid grid-cols-4 gap-4 items-start absolute bottom-0 left-0 right-0 py-10">
     <div>
     <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-        <h1 className="text-sm font-bold">Home</h1>
+        <h1 className="text-sm">Home</h1>
       </a>
-  <a href="/" className="text-black hover:text-indigo-500 hover:underline"> <h1 className="text-sm font-bold">About Us</h1></a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline"> <h1 className="text-sm">About Us</h1></a>
   <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Contact</h1>
+  <h1 className="text-sm">Contact</h1>
   </a>
   <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Treatments</h1>
-  </a>
-  </div>
-  <div>
-  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Patient Info</h1>
-  </a>
-  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Patient Login</h1>
-  </a>
-  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Sitemap</h1>
+  <h1 className="text-sm">Treatments</h1>
   </a>
   </div>
   <div>
   <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Anti-Spam Policy</h1>
+  <h1 className="text-sm">Patient Info</h1>
   </a>
   <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Privacy Policy</h1>
+  <h1 className="text-sm">Patient Login</h1>
   </a>
   <a href="/" className="text-black hover:text-indigo-500 hover:underline">
-  <h1 className="text-sm font-bold">Statement of Accessibility</h1>
+  <h1 className="text-sm">Sitemap</h1>
   </a>
   </div>
   <div>
-  <p>©Copyright FreySmiles 2023 - Braces & Invisalign in Allentown, Bethlehem, Schnecksville, & Lehighton, PA</p>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm">Anti-Spam Policy</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm">Privacy Policy</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm">Statement of Accessibility</h1>
+  </a>
+  </div>
+  <div>
+  <p className = "text-sm">©Copyright FreySmiles 2023 - All Rights Reserved</p>
   </div>
   </div>
   )
