5808e85209ff5b9a10c253e9f573459ca36e7d5b
Jo Lee <jolee224@gmail.com>
2023-04-13 23:08:54 -0400

    homepage and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 const Footer = () => {
-  return <h1 className="text-3xl font-bold underline">Footer</h1>;
+  return (
+  <div className="bg-f7f5f2 grid grid-cols-4 gap-4 items-start absolute bottom-0 left-0 right-0 py-10">
+    <div>
+    <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+        <h1 className="text-sm font-bold">Home</h1>
+      </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline"> <h1 className="text-sm font-bold">About Us</h1></a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Contact</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Treatments</h1>
+  </a>
+  </div>
+  <div>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Patient Info</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Patient Login</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Sitemap</h1>
+  </a>
+  </div>
+  <div>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Anti-Spam Policy</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Privacy Policy</h1>
+  </a>
+  <a href="/" className="text-black hover:text-indigo-500 hover:underline">
+  <h1 className="text-sm font-bold">Statement of Accessibility</h1>
+  </a>
+  </div>
+  <div>
+  <p>©Copyright FreySmiles 2023 - Braces & Invisalign in Allentown, Bethlehem, Schnecksville, & Lehighton, PA</p>
+  </div>
+  </div>
+  )
 };
 
 export default Footer;
