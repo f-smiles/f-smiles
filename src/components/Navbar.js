791aed80b310bb491ebfed192d450818d573e4d0
Wanyi <wanyi.ng1975@gmail.com>
2023-04-16 21:03:33 -0400

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,31 @@ import MobileNav from "./navbar/MobileNav";
 import DesktopNav from "./navbar/DesktopNav";
 
 const Navbar = () => {
-    const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-    useEffect(() => {
-      const handleResize = () => {
-        setIsMobile(window.innerWidth < 768);
-      };
-  
-      window.addEventListener("resize", handleResize);
-  
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }, []);
-  
-    return (
-      <>
-        {isMobile ? <MobileNav /> : <DesktopNav />}
-      </>
-    );
+  useEffect(() => {
+    const checkIsMobile = localStorage.getItem("isMobile");
+    if (checkIsMobile) {
+      setIsMobile(JSON.parse(checkIsMobile));
+    } else {
+      setIsMobile(window.innerWidth < 768);
+    }
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("isMobile", JSON.stringify(isMobile));
+  }, [isMobile]);
+
+  return <>{isMobile ? <MobileNav /> : <DesktopNav />}</>;
 };
 
 export default Navbar;
