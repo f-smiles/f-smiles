9b6d05da3df117f435785903e72673cd6b883bc4
Wanyi <wanyi.ng1975@gmail.com>
2023-04-20 01:35:03 -0400

    style consultation form

--- src/components/patient/VirtualConsultation.js
+++ src/components/patient/VirtualConsultation.js
@@ -6,7 +6,8 @@ import { init } from 'emailjs-com';
 init(process.env.REACT_APP_PUBLIC_KEY);
 
 const VirtualConsultation = () => {
-  const [patient_name, setPatientName] = useState("");
+  const [patient_first_name, setPatientFirstName] = useState("");
+  const [patient_last_name, setPatientLastName] = useState("");
   const [guardian_name, setGuardianName] = useState("");
   const [phone_number, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
@@ -60,7 +61,8 @@ const VirtualConsultation = () => {
       return;
     }
       const templateParams = {
-        patient_name,
+        patient_first_name,
+        patient_last_name,
         guardian_name,
         phone_number,
         email,
@@ -83,14 +85,15 @@ const VirtualConsultation = () => {
           console.log('Failed...', error);
         });
 
-      setPatientName("");
-      setGuardianName("");
-      setPhoneNumber("");
-      setEmail("");
-      const date_of_birth = dateOfBirthRef.current;
-      setPreferredDay("");
-      setPreferredTime("");
-      setMessage("");
+      // setPatientFirstName("");
+      // setPatientLastName("");
+      // setGuardianName("");
+      // setPhoneNumber("");
+      // setEmail("");
+      // const date_of_birth = dateOfBirthRef.current;
+      // setPreferredDay("");
+      // setPreferredTime("");
+      // setMessage("");
       setEmailSent(true);
   };
 
@@ -100,67 +103,81 @@ const VirtualConsultation = () => {
 
 
   return (
-    <main id="contact-form">
+    <main id="contact-form" className="bg-violet-300 p-8">
       {emailSent ? (
         <span className={emailSent ? "block" : "hidden"}>
         Thank you for your message, we will be in touch in no time!
         </span>
       ) : (
-        <form onSubmit={handleSubmit} className="max-w-screen-sm mx-auto flex flex-col space-y-12 py-16">
-          <section>
-            <label htmlFor="full_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-white">
-              First and Last Name
-            </label>
-            <input
-              type="text"
-              placeholder="First and Last Name"
-              value={patient_name}
-              onChange={(e) => setPatientName(e.target.value)}
-              required
-              className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+        <form onSubmit={handleSubmit} className="backdrop-blur-md bg-white/30 max-w-screen-sm mx-auto flex flex-col space-y-12 p-8 rounded-xl">
+          <h1 className="text-center">Request a Virtual Consultation</h1>
+          <section className="flex gap-2 w-full">
+            <div className="w-1/2">
+              <label htmlFor="first name" className="absolute -mt-2 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  value={patient_first_name}
+                  onChange={(e) => setPatientFirstName(e.target.value)}
+                  required
+                  className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+            </div>
+            <div className="w-1/2">
+              <label htmlFor="last name" className="absolute -mt-2 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  value={patient_last_name}
+                  onChange={(e) => setPatientLastName(e.target.value)}
+                  required
+                  className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+            </div>
           </section>
+
           <section>
-            <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-white">
+            <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
               Name of Parent or Guardian
             </label>
             <input
               type="text"
-              placeholder="Parent or Guardian Name"
               value={guardian_name}
               onChange={(e) => setGuardianName(e.target.value)}
-              className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </section>
-          <section>
-            <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-white">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              placeholder="222-222-2222"
-              value={phone_number}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-              className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </section>
-          <section>
-            <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-white">
-              Email address
-            </label>
-            <input
-              type="email"
-              placeholder="Your email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+          <section className="flex gap-2 w-full">
+            <div className="w-1/2">
+              <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                value={phone_number}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+                className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+            <div className="w-1/2">
+              <label htmlFor="guardian_name" className="absolute -mt-3 ml-4 px-2 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
+                Email address
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
           </section>
 
           <div className="my-4 relative mb-3" data-te-datepicker-init data-te-input-wrapper-init>
-            <input required type="text" className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            <input required type="text" className="peer block min-h-[auto] w-full rounded border-0 bg-bg-gray-50 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             placeholder="Select a date"  ref={dateOfBirthRef}/>
             <label htmlFor="floatingInput"
             className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
@@ -207,18 +224,17 @@ const VirtualConsultation = () => {
           </section>
 
           <section className="flex flex-col space-y-8">
-            <div className="flex flex-col space-y-2">
-              <label>
+            <div className="flex flex-col">
+              <label htmlFor="comments" className="absolute -mt-2 ml-4 px-2 pt-1 block mb-2 text-sm font-medium text-gray-900 dark:text-white bg-gray-50 rounded">
                 Tell us about your concern(s):
               </label>
               <textarea
-                  placeholder="Your message"
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
-                  className="border-2 border-violet-200 rounded-lg p-2"
+                  className="border-2 border-violet-200 rounded-lg px-2 py-4 bg-gray-50"
               ></textarea>
             </div>
-            <button type="submit" onClick={handleSubmit} className="flex self-center rounded-full bg-violet-300 px-4 py-2 text-slate-900 hover:bg-violet-400 hover:text-slate-100">Submit</button>
+            <button type="submit" onClick={handleSubmit} className="flex self-center rounded-full bg-violet-300 px-4 py-2 text-violet-900 hover:bg-violet-400 hover:text-slate-100">Submit</button>
           </section>
       </form>
       )}
