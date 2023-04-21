178207232b4272c6da877d78bf72be7f93d87e7b
Wanyi <wanyi.ng1975@gmail.com>
2023-04-20 22:42:28 -0400

    style Why Choose Us

--- src/components/about/WhyChooseUs.js
+++ src/components/about/WhyChooseUs.js
@@ -1,195 +1,224 @@
 import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 const WhyChooseUs = () => {
   return (
-    <>
-      <h1>Why Choose Us</h1>
-      <p>
-        FreySmiles Orthodontics is a family-owned practice providing
-        unparalleled orthodontic experience to the Lehigh Valley community for
-        over 40 years. At FreySmiles Orthodontics, our orthodontists Dr. Gregg
-        Frey and Dr. Daniel Frey are board certified by the American Board of
-        Orthodontics, which demonstrates their highest commitment to excellence
-        in orthodontics. Board certification is a voluntary achievement that
-        only one in three orthodontists choose to pursue, and it requires a
-        thorough evaluation of an orthodontist's knowledge base and clinical
-        skills. By choosing our board certified orthodontists, you can be
-        assured that you are receiving the highest level of patient care and
-        dedication to the specialty.
-      </p>
-      <p>
-        Experts in Invisalign, Damon Braces, Accelerated Orthodontic Treatment,
-        and low-dose 3D Digital Radiographs, FreySmiles also believes in
-        accessible orthodontic care for everyone and offers flexible,
-        zero-interest payment options with low down payments. They established a
-        non-profit organization called More Than Smiles in 2011, providing
-        orthodontic treatment to deserving young individuals who may not have
-        access to world-class orthodontic care or cannot afford it. If you know
-        someone that could benefit from this gift, please visit the More Than
-        Smiles website for the details on how to nominate a deserving
-        individual. FreySmiles has four convenient locations in the Allentown,
-        Bethlehem, Easton, Schnecksville, and Lehighton communities.
-      </p>
-
-      <div>
-        <table className="border border-black">
-          <tbody>
-            <tr className="border">
-              <td className="border border-black border-l border-t"></td>
-              <td className="border border-black border-t">FreySmiles</td>
-              <td className="border border-black border-t">
-                Other Practitioners
-              </td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l border-t">
-                Cutting-Edge Technology
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black border-t"></td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l">
-                Holistic Approach
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black"></td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l">
-                Comprehensive Care
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black"></td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l">ABO Certified</td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black"></td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l">
-                Over 45 years of experience
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black"></td>
-            </tr>
-            <tr className="border">
-              <td className="border border-black border-l">
-                Financial Options
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-              <td className="border border-black">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="#B17CFF"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+    <div className="w-full px-4 pt-16">
+      <div className="mx-auto w-full max-w-screen-lg rounded-2xl bg-white p-2">
+        <Disclosure defaultOpen={true}>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <h1>Why Choose Us</h1>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 space-y-4 text-sm text-gray-500">
+                <p>FreySmiles Orthodontics is a family-owned practice providing unparalleled orthodontic experience to the Lehigh Valley community for over 40 years. At FreySmiles Orthodontics, our orthodontists Dr. Gregg Frey and Dr. Daniel Frey are board certified by the American Board of Orthodontics, which demonstrates their highest commitment to excellence in orthodontics. Board certification is a voluntary achievement that only one in three orthodontists choose to pursue, and it requires a thorough evaluation of an orthodontist's knowledge base and clinical skills. By choosing our board certified orthodontists, you can be assured that you are receiving the highest level of patient care and dedication to the specialty.</p>
+                <p>Experts in Invisalign, Damon Braces, Accelerated Orthodontic Treatment, and low-dose 3D Digital Radiographs, FreySmiles also believes in accessible orthodontic care for everyone and offers flexible, zero-interest payment options with low down payments. They established a non-profit organization called More Than Smiles in 2011, providing orthodontic treatment to deserving young individuals who may not have access to world-class orthodontic care or cannot afford it. If you know someone that could benefit of this gift, please visit the More Than Smiles website for the details on how to nominate a deserving individual. FreySmiles has four convenient locations in the Allentown, Bethlehem, Easton, Schnecksville, and Lehighton communities.
+                </p>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <h1>FreySmiles Orthodontics vs. Other Practitioners</h1>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500 flex justify-center">
+              <table>
+                <tbody>
+                  <tr>
+                    <td className="p-2"></td>
+                    <td className="p-2 text-center">FreySmiles Orthodontics</td>
+                    <td className="p-2 text-center">
+                      Other Practitioners
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Cutting-Edge Technology
+                    </td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto stroke-red-500">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Holistic Approach
+                    </td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto stroke-red-500">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Comprehensive Care
+                    </td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto stroke-red-500">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>ABO Certified</td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td className="my-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto stroke-red-500">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Over 45 years of experience
+                    </td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto stroke-red-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Financial Options
+                    </td>
+                    <td className="mb-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                    <td>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="#B17CFF"
+                        className="w-6 h-6 mx-auto"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
       </div>
-    </>
+    </div>
   );
 };
 
