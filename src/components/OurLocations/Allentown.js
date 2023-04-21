04228d9043f6b010014957fcd7cc4176c88e7064
Jo Lee <jolee224@gmail.com>
2023-04-21 16:22:38 -0400

    phone icon

--- src/components/OurLocations/Allentown.js
+++ src/components/OurLocations/Allentown.js
@@ -1,37 +1,34 @@
 import React from "react";
 
-
 const Allentown = () => {
   return (
     <>
-    <div>
-      <h2>
-        --
-      </h2>
-      <p>
-      Please fill out our appointment request form to
-            schedule an appointment with a skilled Allentown orthodontist at
-            FreySmiles. Virtual consultations are also available if you prefer.
-            You may also use this form to request a Smile Evaluation if this is
-            your first trip to the orthodontist. During this no-obligation
-            consultation, we can determine whether braces or Invisalign would be
-            best for you. If you happen to reach us after hours, we will follow up
-            with you the next business day.During this
-          consultation, one of our award-winning orthodontists will talk to you
-          about your lifestyle, examine your smile, and make recommendations for
-          orthodontic treatment. The
+      <div>
+        <h2>--</h2>
+        <p>
+          Please fill out our appointment request form to schedule an
+          appointment with a skilled Allentown orthodontist at FreySmiles.
+          Virtual consultations are also available if you prefer. You may also
+          use the form to request a Smile Evaluation if this is your first trip
+          to the orthodontist. During this no-obligation consultation, we can
+          determine whether braces or Invisalign would be best for you. If you
+          happen to reach us after hours, we will follow up with you the next
+          business day.During this consultation, one of our award-winning
+          orthodontists will talk to you about your lifestyle, examine your
+          smile, and make recommendations for orthodontic treatment. The
           orthodontists at FreySmiles are Diamond Plus Invisalign providers.
-          </p>
-    </div>
-         <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-slate-900 transition duration-150 ease-in-out">
+        </p>
+      </div>
+      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-stone-950 transition duration-150 ease-in-out">
         <div>
-        <h2 className="py-4">Allentown</h2>
+          <h2 className="py-4">Allentown</h2>
           <p>
             If you live in the Allentown area and are ready to finally get the
             smile of your dreams, please contact us at our conveniently located
-            Allentown office. Our dedicated staff works hard to ensure happy smiles throughout the
-            orthodontic journey.We can answer any general questions or comments you may have for our 
-            FreySmiles Orthodontic team. 
+            Allentown office. Our dedicated staff works hard to ensure happy
+            smiles throughout the orthodontic journey.We can answer any general
+            questions or comments you may have for our FreySmiles Orthodontic
+            team.
           </p>
         </div>
         <div>
@@ -43,96 +40,138 @@ const Allentown = () => {
             <li>Thurs.: 7:00 AM - 4:30 PM</li>
           </ul>
           <div className="py-4">
-          <address>
-            1251 South Cedar Crest Blvd, Suite 210 Allentown, PA 18103 Phone:
-            (610) 437-4748
-          </address>
-          <h3>Find Us:</h3>
-          <p>google map</p>
+            <address>
+              1251 South Cedar Crest Blvd, Suite 210 Allentown, PA 18103
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                (610) 437-4748
+              </div>
+            </address>
+            <h3>Find Us:</h3>
+            <p>google map</p>
           </div>
         </div>
       </div>
 
-
-      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-slate-900 transition duration-150 ease-in-out">
-      <div>
-      <h2 className="py-4">Lehighton</h2>
-        <p>
-          The experienced Lehighton orthodontists at FreySmiles have
-          transformed thousands of happy smiles through orthodontic
-          treatment. We are dedicated to providing our patients with the
-          highest quality braces and Invisalign treatment and beautiful
-          results. Our FreySmiles Orthodontic Office in Lehighton is conveniently
-          located, minutes away from Jim Thorpe and Tamaqua. 
-        </p>
-      </div>
-      <div>
-        <h3>Hours of Operation</h3>
-        <ul>
-          <li>Mon.: 11:00 AM - 7:00 PM</li>
-          <li>Tues.: 11:00 AM - 7:00 PM</li>
-          <li>Wed.: 8:00 AM - 5:30 PM</li>
-          <li>Thurs.: 7:00 AM - 4:30 PM</li>
-        </ul>
-        <div className="py-4">
-        <address>
-          1080 Blakeslee Blvd Dr E Lehighton, PA 18235 Phone: (610) 437-4748
-        </address>
-        <h3>Find Us:</h3>
-        <p>google map</p>
-      </div>
-      </div>
-  </div>
-
-  <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-slate-900 transition duration-150 ease-in-out">
-      <div>
-      <h2 className="py-4">Bethlehem</h2>
-      <p>
-        Bethlehem is known for more than its robust historic district, its
-        industrial history, and the Christmas Tree that lights up the Lehigh
-        Valley. This city is also known for the beautiful smiles and straight
-        teeth crafted by Dr. Gregg Frey and Dr. Daniel Frey at FreySmiles
-        Orthodontics. FreySmiles Orthodontics has provided thousands of patients with happy,
-          beautiful smiles throughout Bethlehem and Easton. We
-          are extremely proud to be the region’s top Invisalign and Invisalign
-          Teen provider. 
-        </p>
-        <h3>Bethlehem Office Virtual Tour</h3>
-        <p>
-          We invite you to take a virtual tour of the FreySmiles orthodontic
-          office in Bethlehem, PA. You can explore our waiting room, our
-          innovative technology, and our workspaces where we make smiles
-          beautiful.
-        </p>
-      </div>
-      <div>      
-        <h3>Hours of Operation</h3>
-        <ul>
-          <li>Mon.: 11:00 AM - 7:00 PM</li>
-          <li>Tues.: 11:00 AM - 7:00 PM</li>
-          <li>Wed.: 8:00 AM - 5:30 PM</li>
-          <li>Thurs.: 7:00 AM - 4:30 PM</li>
-        </ul>
-        <div className="py-4">
-        <address>
-          2901 Emrick Boulevard Bethlehem, PA 18020 Phone: (610) 437-4748
-        </address>
-        <h3>Find Us:</h3>
-        <p>google map</p>
+      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-stone-950 transition duration-150 ease-in-out">
+        <div>
+          <h2 className="py-4">Lehighton</h2>
+          <p>
+            The experienced Lehighton orthodontists at FreySmiles have
+            transformed thousands of happy smiles through orthodontic treatment.
+            We are dedicated to providing our patients with the highest quality
+            braces and Invisalign treatment and beautiful results. Our
+            FreySmiles Orthodontic Office in Lehighton is conveniently located,
+            minutes away from Jim Thorpe and Tamaqua.
+          </p>
         </div>
+        <div>
+          <h3>Hours of Operation</h3>
+          <ul>
+            <li>Mon.: 11:00 AM - 7:00 PM</li>
+            <li>Tues.: 11:00 AM - 7:00 PM</li>
+            <li>Wed.: 8:00 AM - 5:30 PM</li>
+            <li>Thurs.: 7:00 AM - 4:30 PM</li>
+          </ul>
+          <div className="py-4">
+            <address>
+              1080 Blakeslee Blvd Dr E Lehighton, PA 18235
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>{" "}
+                (610) 437-4748
+              </div>
+            </address>
+            <h3>Find Us:</h3>
+            <p>google map</p>
+          </div>
         </div>
       </div>
 
-      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-slate-900 transition duration-150 ease-in-out">
-      <div>
-      <h2 className="py-4">Schnecksville</h2>
+      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-stone-950 transition duration-150 ease-in-out">
+        <div>
+          <h2 className="py-4">Bethlehem</h2>
+          <p>
+            Bethlehem is known for more than its robust historic district, its
+            industrial history, and the Christmas Tree that lights up the Lehigh
+            Valley. This city is also known for the beautiful smiles and
+            straight teeth crafted by Dr. Gregg Frey and Dr. Daniel Frey at
+            FreySmiles Orthodontics. FreySmiles Orthodontics has provided
+            thousands of patients with happy, beautiful smiles throughout
+            Bethlehem and Easton. We are extremely proud to be the region’s top
+            Invisalign and Invisalign Teen provider.
+          </p>
+          <h3>Bethlehem Office Virtual Tour</h3>
+          <p>
+            We invite you to take a virtual tour of the FreySmiles orthodontic
+            office in Bethlehem, PA. You can explore our waiting room, our
+            innovative technology, and our workspaces where we make smiles
+            beautiful.
+          </p>
+        </div>
+        <div>
+          <h3>Hours of Operation</h3>
+          <ul>
+            <li>Mon.: 11:00 AM - 7:00 PM</li>
+            <li>Tues.: 11:00 AM - 7:00 PM</li>
+            <li>Wed.: 8:00 AM - 5:30 PM</li>
+            <li>Thurs.: 7:00 AM - 4:30 PM</li>
+          </ul>
+          <div className="py-4">
+            <address>
+              2901 Emrick Boulevard Bethlehem, PA 18020
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>{" "}
+                (610) 437-4748
+              </div>
+            </address>
+            <h3>Find Us:</h3>
+            <p>google map</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="border grid grid-cols-3 gap-4 cursor-pointer hover:text-white hover:bg-stone-950 transition duration-150 ease-in-out">
+        <div>
+          <h2 className="py-4">Schnecksville</h2>
           <p>
             Are you ready to join the thousands of beautiful smiles transformed
             through orthodontic treatment? At FreySmiles Orthodontics, our
             experienced orthodontists give patients lifelong confidence through
             braces and Invisalign treatment.
           </p>
-          
         </div>
         <div>
           <h3>Hours of Operation</h3>
@@ -143,12 +182,27 @@ const Allentown = () => {
             <li>Thurs.: 7:00 AM - 4:30 PM</li>
           </ul>
           <div className="py-4">
-          <address>
-            4155 Independence Drive Schnecksville, PA 18078 Phone: (610) 437-4748
-          </address>
-          <h3>Find Us:</h3>
-          <p>google map</p>
-        </div>
+            <address>
+              4155 Independence Drive Schnecksville, PA 18078
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-4 h-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>{" "}
+                (610) 437-4748
+              </div>
+            </address>
+            <h3>Find Us:</h3>
+            <p>google map</p>
+          </div>
         </div>
       </div>
     </>
