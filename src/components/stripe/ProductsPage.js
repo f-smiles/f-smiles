21fae3882df99ce58a012b9b44d54281e32d6cc0
Jo Lee <jolee224@gmail.com>
2023-05-02 22:19:14 -0400

    local storage

--- src/components/stripe/ProductsPage.js
+++ src/components/stripe/ProductsPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ProductsArray, getProductData } from "./products";
 import Cart from "./Cart";
@@ -7,10 +7,24 @@ const ProductsPage = () => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
 
+  useEffect(() => {
+    const savedCart = JSON.parse(localStorage.getItem("cart"));
+    if (savedCart) {
+      setCart(savedCart);
+      const savedTotal = savedCart.reduce(
+        (accumulator, current) => accumulator + current.price,
+        0
+      );
+      setTotal(savedTotal);
+    }
+  }, []);
+
   const addToCart = (productId) => {
     const productData = getProductData(productId);
     if (productData !== undefined) {
-      setCart([...cart, productData]);
+      const updatedCart = [...cart, productData];
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
       setTotal(total + productData.price);
     }
   };
@@ -20,6 +34,7 @@ const ProductsPage = () => {
     const removedProduct = cart.find((product) => product.id === productId);
     if (removedProduct !== undefined) {
       setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
       setTotal(total - removedProduct.price);
     }
   };
@@ -33,17 +48,14 @@ const ProductsPage = () => {
             <div key={product.id}>
               <div>
                 <Card
-                id={product.id}
+                  id={product.id}
                   name={product.name}
                   image={product.image}
                   price={product.price}
-                  // description={product.description}
                   addToCart={() => addToCart(product.id)}
                 />
               </div>
-              <div>
-              
-              </div>
+              <div></div>
             </div>
           );
         })}
@@ -58,28 +70,28 @@ const Card = ({id, image, name, description, price, addToCart }) => {
   const [hover, setHover] = useState(false)
   
   return (
-    <Link to={`/products/${id}`}>
     <div
       data-aos="fade-up"
       data-aos-duration="750"
       data-aos-easing="linear"
       className="bg-F1F1F0 max-w-xs px-12 pb-12 pt-20 flex flex-col"
     >
-      <div className="grid grid-rows-2 gap-4">
-        <img
-          className="h-32 mx-auto my-auto self-center"
-          src={image}
-          alt={name}
-        />
-        <div>
-          <h3 className="uppercase mb-4">{name}</h3>
-          <h3 className="uppercase mb-4">{price}</h3>
-          {/* <p>{description}</p> */}
-          <button className="border border-gray-900 text-gray-900 py-2 px-4 rounded-full" onClick={addToCart}>Add To Cart</button>
+      <Link to={`/products/${id}`}>
+        <div className="grid grid-rows-2 gap-4">
+          <img
+            className="h-32 mx-auto my-auto self-center"
+            src={image}
+            alt={name}
+          />
+          <div>
+            <h3 className="uppercase mb-4">{name}</h3>
+            <h3 className="uppercase mb-4">{price}</h3>
+            {/* <p>{description}</p> */}
+          </div>
         </div>
-      </div>
+      </Link>
+      <button className="border border-gray-900 text-gray-900 py-2 px-4 rounded-full" onClick={addToCart}>Add To Cart</button>
     </div>
-    </Link>
   );
 };
 
--- src/components/stripe/SingleProductPage.js
+++ src/components/stripe/SingleProductPage.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { getProductData } from "./products";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const SingleProductPage = () => {
   const { id } = useParams();
   const productData = getProductData(id);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
 
   if (!productData) {
     return <div>Product not found</div>;
   }
 
+  const addToCart = (productId) => {
+    const productData = getProductData(productId);
+    if (productData !== undefined) {
+      setCart([...cart, productData]);
+      setTotal(total + productData.price);
+    }
+  };
+
   return (
     <div className="mt-40">
+      <div key={productData.id} className="grid grid-cols-2 gap-4">
       <h1>{productData.name}</h1>
       <img src={productData.image} alt={productData.name} />
       <p>{productData.description}</p>
       <p>{productData.price}</p>
-      <button>Add to cart</button>
+      <button onClick={() => addToCart(productData.id)}>Add to cart</button>
+    </div>
     </div>
   );
 };
