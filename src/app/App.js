ff71e18409ab6118fe763715f482b9a30820eb56
Jo Lee <jolee224@gmail.com>
2023-05-09 22:29:39 -0400

    cartcount

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import AppRoutes from './AppRoutes';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function App() {
-  
+  const [cartCount, setCartCount] = useState(0);
+
   return (
     <>
-    <Navbar />
-    <AppRoutes />
-    <Footer />
+      <Navbar cartCount={cartCount} />
+      <AppRoutes setCartCount={setCartCount} />
+      <Footer />
     </>
   );
 }
 
 export default App;
+
