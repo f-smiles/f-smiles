3d0e353cb955446c14d13d73ee2a372c1e89541d
Jo Lee <jolee224@gmail.com>
2023-05-16 23:15:15 -0400

    navbar transparency

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,6 @@ export default function DesktopNavbar() {
       const scrollTop = window.pageYOffset;
       const threshold = 10; 
 
-
       if (scrollTop > threshold) {
         setNavbarTransparent(false);
       } else {
