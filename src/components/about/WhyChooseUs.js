fcdb4f2c8cff520a15b03f0eb03bde3b0389de68
Jo Lee <jolee224@gmail.com>
2023-06-05 12:58:11 -0400

    accordion

--- src/components/about/WhyChooseUs.js
+++ src/components/about/WhyChooseUs.js
@@ -1,15 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
-
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
 
 const WhyChooseUs = () => {
-
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const words = ["Invisalign", "Damon Braces", "Accelerated Orthodontic Treatment", "low-dose 3D Digital Radiographs"];
+  const words = [
+    "Invisalign",
+    "Damon Braces",
+    "Accelerated Orthodontic Treatment",
+    "low-dose 3D Digital Radiographs",
+  ];
+
+  const [open, setOpen] = useState(1);
+  const [alwaysOpen, setAlwaysOpen] = useState(true);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,10 +34,8 @@ const WhyChooseUs = () => {
     return () => clearInterval(interval);
   }, [words.length]);
 
-  
   return (
-    
-    <div className="bg-indigo-50 py-4  w-full px-4 pt-16">
+    <div className="bg-gradient-to-r from-neutral-50 to-teal-50 to-violet-100 to-violet-200 py-4  w-full px-4 pt-16">
       <img
         src="../../images/pinkbanner.png"
         alt="invisalign"
@@ -37,14 +50,16 @@ const WhyChooseUs = () => {
       />
       <div className=" relative rounded-lg px-4 pt-4 pb-2 space-y-4 text-gray-500 text-center relative z-10">
         <div>
-        <h1 className="mt-20 mb-20 text-white text-5xl relative">
+          <h1 className="mt-20 mb-20 text-stone-100 text-5xl relative">
             <span className="mr-4 justify-start -ml-80">Experts in</span>{" "}
             <span className="relative">
               {words.map((word, index) => (
                 <span
                   key={index}
                   className={`transition-opacity duration-1000 ${
-                    index === currentWordIndex ? "opacity-100 text-violet-700" : "opacity-0 text-violet-700"
+                    index === currentWordIndex
+                      ? "opacity-100 text-violet-700"
+                      : "opacity-0 text-violet-700"
                   }`}
                   style={{
                     position: "absolute",
@@ -62,14 +77,14 @@ const WhyChooseUs = () => {
               ))}
             </span>
           </h1>
-  </div>
+        </div>
       </div>
       <div className="mx-auto w-full max-w-screen-lg rounded-2xl p-2">
         <div className="relative">
           <div className="absolute inset-0 flex justify-center items-center"></div>
         </div>
-        <div className="rounded-full relative bg-gradient-to-b from-stone-50 to-stone-50 overflow-hidden">
-          <p className="mt-8 text-center px-12 py-8 mx-auto text-md text-amber-500 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+        <div className="rounded-full relative bg-gradient-to-r from-rose-50 to-neutral-50 to-violet-50 to-teal-50 overflow-hidden">
+          <p className="mt-16 text-center px-12 py-8 mx-auto text-md text-violet-800 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
             FreySmiles Orthodontics is a family-owned practice providing
             unparalleled orthodontic experience to the Lehigh Valley community
             for over four decades.{" "}
@@ -78,60 +93,109 @@ const WhyChooseUs = () => {
               Our office holds the distinction of being the longest-standing,
               active board-certified orthodontic office in the area.{" "}
             </span>{" "}
-            we strive to treat attain finished results consistent with the
-            American Board of Orthodontics (ABO) qualitative standards. Our
-            dedicated doctors place great priority on the certification and
-            recertification process, ensuring that all diagnostic records adhere
-            to the ABO standards. Currently, Dr. Gregg Frey, a certified
-            orthodontist, is actively preparing cases for recertification. Dr.
-            Daniel Frey is in the final stages of obtaining his initial
-            certification. To complement our use of cutting-edge diagnostic
-            technology, we uphold the highest standards for our records,
-            ensuring accuracy and precision throughout the treatment process.
+            <p className="mt-7">
+              {" "}
+              We strive to attain finished results consistent with the
+              <span className="text-2xl">
+                {" "}
+                American Board of Orthodontics (ABO){" "}
+              </span>{" "}
+              qualitative standards. Our dedicated doctors place great priority
+              on the certification and recertification process, ensuring that
+              all diagnostic records adhere to ABO standards. Currently, Dr.
+              Gregg Frey is a certified orthodontist, and is preparing cases for
+              recertification. Dr. Daniel Frey is in the final stages of
+              obtaining his initial certification. To complement our use of
+              cutting-edge diagnostic technology, we uphold the highest
+              standards for our records, ensuring accuracy and precision
+              throughout the treatment process.
+            </p>
           </p>
 
-          <p className="mb-10 ml-10 mr-10 text-center px-4 py-2 mx-auto text-md text-amber-600 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 whitespace-[60ch]">
+          <p className="mb-14 ml-10 mr-10 text-center px-4 py-2 mx-auto text-md text-violet-800 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 whitespace-[60ch]">
             Our office is the #1 provider of Invisalign and Invisalign Teen in
             the region.{" "}
             <span className="text-2xl">
-              As Diamond Plus Invisalign providers
+              As Diamond Plus Invisalign providers,
             </span>{" "}
-            Their care over the many years has contributed to the research and
+            their care over the many years has contributed to the research and
             advancement of this specialized orthodontic treatment modality.
           </p>
         </div>
-       
-      <NavLink
+
+        <NavLink
           to="/virtual-consultation"
-          className="-ml-20 mt-10 curspor-pointer px-5 py-5 cursor-pointer bg-teal-800 rounded-full px-8 py-2 hover:bg-teal-500 text-lg text-white tracking-normal text-white hover:text-white z-20 relative"
+          className="-ml-20 mt-10 curspor-pointer px-5 py-5 cursor-pointer bg-teal-800 rounded-full px-8 py-2 hover:bg-emerald-700 text-lg text-white tracking-normal text-white hover:text-white z-20 relative"
           style={{ zIndex: "20" }}
         >
           <button className="mt-20">Schedule a Consultation</button>
         </NavLink>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "flex-end",
-          }}
+        <div style={{ display: "flex", alignItems: "flex-start" }}>
+  <div style={{ display: "flex", flexDirection: "column" }} className="mt-10 -ml-10 text-violet-800">
+    <Fragment>
+      <Accordion open={open === 1}>
+        <h1>
+        <AccordionHeader onClick={() => handleOpen(1)} className="text-3xl border-b-0">
+        <span style={{ whiteSpace: "nowrap" }}>Meet Our Doctors</span> +
+        </AccordionHeader>
+        </h1>
+        <AccordionBody>
+          
+        <ul>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/our-team">Dr. Gregg Frey</a>
+    </li>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/our-team">Dr. Daniel Frey</a>
+    </li>
+  </ul>
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 2}>
+        <h1>
+        <AccordionHeader onClick={() => handleOpen(2)} className="text-3xl border-b-0">
+        <span style={{ whiteSpace: "nowrap" }}>4 Locations</span> +
+        </AccordionHeader>
+        </h1>
+        <AccordionBody>
+        <ul>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/locations">Allentown</a>
+    </li>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/locations">Bethlehem</a>
+    </li>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/locations">Lehigton</a>
+    </li>
+    <li>
+      <a className="text-lg underline cursor-pointer text-indigo-500 hover:text-indigo-700" href="/locations">Schnecksville</a>
+    </li>
+  </ul>
+        </AccordionBody>
+      </Accordion>
+    </Fragment>
+  </div>
+  <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "flex-end" }}>
+    <img
+      src="../../images/drfreyperfecting.jpg"
+      alt="invisalign"
+      className="opacity-90 mt-4"
+      style={{
+        width: "50%",
+        height: "auto",
+      }}
+    />
+  </div>
+</div>
+
+        <p
+          className="bg-stone-100 text-violet-800  text-lg mt-10 mr-20 relative text-center"
+          style={{ zIndex: "20", flex: "1" }}
         >
-          <img
-            src="../../images/drfreyperfecting.jpg"
-            alt="invisalign"
-            className="opacity-90 mt-10"
-            style={{
-              width: "50%",
-              height: "auto",
-            }}
-          />
-        </div>
-        <div className="px-4 py-2 mx-auto text-2xl text-stone-800">
-      
-      </div>
-        <p className="mt-10 relative" style={{ zIndex: "20" }}>
           FreySmiles believes in accessible orthodontic care for everyone and
-          offers flexible, zero-interest payment options with low down payments.
-          They established a non-profit organization called{" "}
+          offers flexible payment options. They established a non-profit
+          organization called{" "}
           <span>
             {" "}
             <a
@@ -143,12 +207,14 @@ const WhyChooseUs = () => {
           </span>{" "}
           in 2011, providing orthodontic treatment to deserving young
           individuals who may not have access to world-class orthodontic care or
-          cannot afford it. If you know someone that could benefit of this gift,
-          please visit the website for the details on how to nominate a
-          deserving individual. FreySmiles has four convenient locations in the
-          Allentown, Bethlehem, Easton, Schnecksville, and Lehighton
-          communities.
+          cannot afford it. If you know someone that could benefit from this
+          gift, please visit the website for details on how to nominate a
+          deserving individual.
         </p>
+
+        <div className="px-4 py-2 mx-auto text-xl text-stone-800">
+          <p className="mt-1 relative" style={{ zIndex: "20" }}></p>
+        </div>
 
         <table
           className="mx-auto"
