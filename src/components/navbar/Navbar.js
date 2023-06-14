e0d5eee25af717bd4a7e4835b67b806edf9a9526
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-06-13 22:04:12 -0400

    edit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -430,15 +430,15 @@ className={
           onClick={handleToggleSidebar}
         >
           <span className="hover:text-violet-500">Bag</span>
-          <div className="relative flex items-center">
+          <div className="relative">
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
               <path d="M17.54 5.424a.47.47 0 0 1 .46.474v17.627a.47.47 0 0 1-.46.475H.46a.47.47 0 0 1-.46-.475V5.898a.47.47 0 0 1 .46-.474h4.795v-1.56C5.255 1.733 6.935 0 9 0c2.065 0 3.745 1.733 3.745 3.864v1.56zm-11.365 0h5.64v-1.56c0-1.608-1.264-2.915-2.82-2.915-1.555 0-2.82 1.307-2.82 2.915zm10.905.949h-4.335V8.61a.47.47 0 0 1-.46.475.47.47 0 0 1-.46-.475V6.373h-5.65V8.61a.47.47 0 0 1-.46.475.47.47 0 0 1-.46-.475V6.373H.92V23.05h16.16z"></path>
             </svg>
-            <span className="absolute top-0 right-0 text-black text-xs px-1 rounded-full mt-2 mr-2">
+            <span className="absolute top-1/2 left-1/2 -translate-x-3/4 -translate-y-1/3 text-black text-xs rounded-full">
               {cartCount}
             </span>
           </div>
