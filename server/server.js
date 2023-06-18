899abdecf2dfa5b3bf99a954e3e655f8b05bbbcf
Wanyi <wanyi.ng1975@gmail.com>
2023-06-18 17:03:34 -0400

    rework app server setup

--- server/server.js
+++ server/server.js
@@ -1,59 +1,27 @@
 const express = require("express");
-const app = express();
-app.use(express.static('public'));
-
+const cors = require('cors');
 require("dotenv").config();
 
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_TEST_KEY);
+const app = express();
 
-const bodyParser = require('body-parser');
-const { resolve } = require("path");
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-// const stripe = require("stripe")(process.env.REACT_APP_STRIPE_KEY, {
-//   apiVersion: "2022-11-15",
-// });
+app.use('/api/prices', require('./api/prices'));
+app.use('/api/products', require('./api/products'));
 
-// const path = require('path');
-// const publicDirectoryPath = path.join(__dirname, 'public');
-// app.use(express.static(publicDirectoryPath));
+app.use('/api', (req, res) => {
+  res.send('/api connected!')
+})
 
-app.get("/", (req, res) => {
-  const path = resolve(publicDirectoryPath + "/index.html");
-  res.sendFile(path);
-});
-
-app.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY,
-  });
+// error-handing middleware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
 });
 
 
-app.post('/api/payments', async (req, res) => {
-  try {
-    const { token, paymentIntent, customer } = req.body;
-    const intent = await stripe.paymentIntents.retrieve(paymentIntent);
-    const paymentMethod = await stripe.paymentMethods.attach(token.card.id, { customer: customer });
-
-    const confirmPaymentIntent = await stripe.paymentIntents.confirm(paymentIntent, {
-      payment_method: paymentMethod.id
-    });
-
-    if (confirmPaymentIntent.status === "succeeded") {
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
-});
-
-
-
-
-app.listen(3000, () =>
-  console.log(`Node server listening at http://localhost:3000/api/payments`)
-);
+const PORT = process.env.REACT_APP_PORT || 8080;
+app.listen(PORT, () => console.log(`Node server listening at port ${PORT}`));
