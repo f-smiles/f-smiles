33af9a8a8e3fbdda970c780bc0ec87067f498ec7
holycaca0 <jolee224@gmail.com>
2023-06-23 18:53:37 -0400

    nothing changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -131,8 +131,8 @@ const Home = () => {
                   Our goal is to make your smile look best on{" "}
                   <strong className="font-bold">YOU</strong>. It's an art, it's
                   a science, and it is something orthodontists Dr. Gregg Frey
-                  and Dr. Daniel Frey and the exceptional team at FreySmiles
-                  Orthodontics recognize and are very passionate about.
+                  and Dr. Daniel Frey and the exceptional steam at FreySmiles
+                  Orthodontics recognize and are very passionate about. 
                 </p>
                 {/* <div className="absolute top-2/3 right-2/3 border border-black w-1/2">
                     <img
