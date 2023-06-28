8f57b0858cc744b39eff34a62d55009f2b2c1c39
holycaca0 <jolee224@gmail.com>
2023-06-27 22:27:29 -0400

    review carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,22 +6,58 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import LogoSlider from "./logoslider";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
-
+import { Carousel } from "@material-tailwind/react";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 AOS.init();
 
 const Home = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const slides = [
+    {
+      image: 'image1.jpg',
+     author: 'Taneesha N.',
+      content: `Fantastic!! Absolutely recommend! The team at FreySmiles was extremely accommodating to my busy schedule and was able to get me an appointment the day after I called. The staff was so sweet and made me feel welcomed as soon as I walked into the office.  Dr. Frey has a wonderful practice. I have all the confidence in the world that I will love my smile once my treatment is done!`,
+    },
+    {
+      image: 'image2.jpg',
+      author:  "Alexandra W",
+      content: '“My husband’s experience at FreySmiles Orthodontics has been absolutely amazing. He had an emergency that occurred on Friday and contacted the emergency contact number. Soon after, he was contacted by Dr. Gregg Frey who scheduled him an appointment right away. My husband was so happy to not have to go through the weekend in discomfort. Hands down, I have to say this is the best Orthodontist we have ever been to. The staff are always professional, polite and very eager to help!”',
+    },
+    {
+      image: 'image3.jpg',
+      author: "Fei Z",
+      content: 'Our whole experience for the past 10 years of being under Dr. Gregg Frey’s care and his wonderful staff has been amazing. My son and my daughter have most beautiful smiles, and they received so many compliments on their teeth. It has made a dramatic and positive change in their lives. Dr. Frey is a perfectionist, and his treatment is second to none. I recommend Dr. Frey highly and without any reservation.',
+    },
+  ];
+
+  const prevSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+  };
+
+  const nextSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+  };
   const [isCardHovered, setIsCardHovered] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [isTransition, setIsTransition] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(0);
-  
+  const [showTextPerson1, setShowTextPerson1] = useState(true);
+  const [showTextPerson2, setShowTextPerson2] = useState(false);
+
+  const handleToggleTextPerson1 = () => {
+    setShowTextPerson1(!showTextPerson1);
+  };
+
+  const handleToggleTextPerson2 = () => {
+    setShowTextPerson2(!showTextPerson2);
+  };
+
   const threshold = 400;
   const InvisalignRef = useRef(null);
   const damonRef = useRef(null);
   const techRef = useRef(null);
 
-
-  
   useEffect(() => {
     const handleScroll = () => {
       const position = window.pageYOffset;
@@ -52,20 +88,10 @@ const Home = () => {
     return sectionNumber * scrollPosition * 0.5;
   };
 
-  const handleClickScroll = () => {
-    const jumpToSection = document.querySelector("#invisalign-damon");
-    if (jumpToSection) {
-      jumpToSection.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <>
       <main className="pt-16 bg-#fFFFDFD overflow-hidden w-full">
-      <section
-          id="hero-section"
-          className="max-w-screen-lg mx-auto py-8"
-        >
+        <section id="hero-section" className="max-w-screen-lg mx-auto py-8">
           <div className="flex flex-col-reverse md:flex-row">
             <div className="mt-24 md:mt-0 flex flex-col px-4 md:w-1/2 lg:px-0">
               <div className="p-8 lg:p-0">
@@ -77,7 +103,7 @@ const Home = () => {
                   <strong className="font-bold">YOU</strong>. It's an art, it's
                   a science, and it is something orthodontists Dr. Gregg Frey
                   and Dr. Daniel Frey and the exceptional team at FreySmiles
-                  Orthodontics recognize and are very passionate about. 
+                  Orthodontics recognize and are very passionate about.
                 </p>
                 {/* <div className="absolute top-2/3 right-2/3 border border-black w-1/2">
                     <img
@@ -121,116 +147,183 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="scroll-down-btn flex flex-col justify-center items-center mt-8">
-            <button
-              className="animate-bounce flex flex-col justify-center items-center"
-              type="button"
-              onClick={handleClickScroll}
-            >
-              <h6 className="text-violet-500">Scroll Down</h6>
-              <ChevronDownIcon className="w-6 h-6 text-violet-500" />
-            </button>
-          </div>
         </section>
         <section
           style={{
             transform: `translateY(-${calculateOffset(1)}px)`,
             backgroundColor: isCardHovered ? "#dec0ae" : "#F8F6F1",
-            transition: "background-color 0.7s ease"
+            transition: "background-color 0.7s ease",
           }}
           className="bg-F8F6F1"
         >
-  <div className="p-48">
-    <div className="flex">
-      <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
-        <h3 className="text-6xl">Invisalign</h3>
-        <p className="md:px-8">
-          As part of the top 1% of Invisalign providers in the US, we have the
-          experience to deliver the smile you deserve.
-        </p>
-      </div>
-      <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
-        <img className="rounded-lg self-center" src="../images/aligner.png" alt="invis" />
-      </div>
-      <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
-        <button className="rounded-full border border-black px-6 py-4">
-          <Link to="/invisalign">How Invisalign Works</Link>
-        </button>
-      </div>
-    </div>
-  </div>
-  <div className="flex">
-    <img
-      className="rounded-lg self-center mx-auto"
-      src="../images/damonfull.png"
-      alt="invis"
-    />
-    <div  ref={damonRef} className="text-center flex flex-col gap-6 justify-center items-center w-1/2">
-      <img src="../images/damontech.png" alt="damon braces" />
-      <h3 className="text-2xl">Damon Braces</h3>
-      <p className="md:px-8">
-        Combining self-ligating braces with advanced archwires clinically proven to move teeth quickly and comfortably.
-      </p>
-      <button className="rounded bg-violet-200 px-6 py-4">
-        <Link to="/braces">How Damon Braces Work</Link>
-      </button>
-    </div>
-  </div>
-  <div>
+          <div >
+            <div className="flex">
+              <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
+                <h3 className="text-4xl">Invisalign</h3>
+                <p className="md:px-8">
+                  As part of the top 1% of Invisalign providers in the US, we
+                  have the experience to deliver the smile you deserve.
+                </p>
+              </div>
+              <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
+                <img
+                  className="rounded-lg self-center"
+                  src="../images/aligner.png"
+                  alt="invis"
+                />
+              </div>
+              <div className="text-center flex flex-col gap-6 justify-center items-center w-1/3">
+                <button className="rounded border border-black px-6 py-4">
+                  <Link to="/invisalign">How Invisalign Works</Link>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <img
+              className="rounded-lg self-center mx-auto"
+              src="../images/damonfull.png"
+              alt="invis"
+              style={{ width: "30%", height: "auto" }}
+            />
+
+            <div
+              ref={damonRef}
+              className="text-center flex flex-col gap-6 justify-center items-center w-1/2"
+            >
+              <img
+                src="../images/damontech.png"
+                style={{ width: "30%", height: "auto" }}
+                alt="damon braces"
+              />
+              <h3 className="text-4xl">Damon Braces</h3>
+              <p className="md:px-8">
+                Combining self-ligating braces with advanced archwires
+                clinically proven to move teeth quickly and comfortably.
+              </p>
+              <button className="rounded border border-black px-6 py-4">
+                <Link to="/braces">Damon System </Link>
+              </button>
+            </div>
+          </div>
+          <div className="pt-48 flex">
+            <div className="w-1/3">
               <img
                 className="rounded-lg h-96 w-auto self-center mx-auto"
-                src="../images/boytero.jpg"
+                src="../images/itero2.png"
                 alt="invis"
               />
-              <div className="text-center flex flex-col gap-6 justify-center items-center w-1/2">
-                <img  src="../images/technology.png" alt="itero" />
-                <h3 className="text-2xl">Advanced Technology</h3>
+            </div>
+            <div className="w-1/3">
+              <div className="text-center flex flex-col gap-6 justify-center items-center">
+                <h3 className="text-4xl">Advanced Technology</h3>
                 <p className="md:px-8">
-                  {" "}
                   We offer Invisalign without Impressions. Say goodbye to goopy
                   impressions with our iTero digital scanner.
                 </p>
-                <button className="rounded bg-violet-200 px-6 py-4">
+                <img src="../images/technology.png" alt="itero" />
+
+                <button className="rounded border border-black px-6 py-4">
                   <Link to="/invisalign">Learn More</Link>
                 </button>
               </div>
             </div>
-</section>
-
-
-
-
-        <div className="mt-10 mb-60">
+          </div>
+          <div className="mt-10">
           <LogoSlider style={{ transform: "translate(-50%, -50%)" }} />
         </div>
+        </section>
 
         <div className="relative">
-          <div className=" absolute -top-60 right-0 z-10">
-            <img
-              className="mt-20 opacity-20"
-              style={{ height: "280px", width: "auto" }}
-              src="../images/chat.png"
-              alt="technology"
-            />
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={`absolute left-0 top-0 w-full h-full transition-opacity duration-500 ${
+            index === activeSlide ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <img src={slide.image} className="w-full" alt={slide.title} />
+          <div className="absolute inset-x-0 bottom-0 p-4 text-black">
+            <h5 className="text-xl font-bold">{slide.title}</h5>
+            <p>{slide.content}</p>
           </div>
-          <p
-            className=" text-teal-500 text-center absolute -bottom-3  md:right-12 z-10"
-            style={{
-              maxWidth: "300px",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-            }}
-          >
-            I have never been to a doctor's office that is anything like Dr.
-            Frey's. Dr. Frey goes above and beyond to create the best smiles and
-            has by far the nicest staff around.
-          </p>
-
-          <h1 className="text-teal-500 text-xl text-right absolute -bottom-40 md:right-60 z-10">
-            Rebecca M.
-          </h1>
         </div>
+      ))}
+      <button
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 rounded"
+        onClick={prevSlide}
+      >
+           <div className="border border-black rounded-full inline-flex p-1">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+</svg>
 
+</div>
+            
+      </button>
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 rounded"
+        onClick={nextSlide}
+      >
+   <div className="border border-black rounded-full inline-flex p-1">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 rounded-full">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+  </svg>
+</div>
+      </button>
+    </div>
+
+
+        <section>
+         
+
+      <div className="flex justify-center">
+        <div className="flex justify-center">
+          <div className="flex items-center">
+            <div className="flex flex-col items-center">
+              {showTextPerson1 && (
+                <p className="mt-10">
+                 
+
+                </p>
+              )}
+              {!showTextPerson1 && showTextPerson2 && (
+                <p className="mt-10">
+                  Paragraph for Person 2.
+                </p>
+              )}
+              <h1
+                onClick={handleToggleTextPerson1}
+                className="text-teal-500 text-xl mb-2 cursor-pointer"
+              >
+                Person 1
+              </h1>
+              <div className="bg-green-500 h-1 w-80"></div>
+            </div>
+            <div className="w-2"></div>
+            <div className="flex flex-col items-center">
+              <h1
+                onClick={handleToggleTextPerson2}
+                className="text-teal-500 text-xl mb-2 cursor-pointer"
+              >
+                Person 2
+              </h1>
+              {showTextPerson2 && (
+                <p className="mt-10">
+                  Additional information for Person 2.
+                </p>
+              )}
+              <div className="bg-green-500 h-1 w-80"></div>
+            </div>
+            <div className="w-2"></div>
+            <div className="flex flex-col items-center">
+              <h1 className="text-teal-500 text-xl">Person 3</h1>
+              <div className="bg-green-500 h-1 w-80"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
         <section className="max-w-screen-xl mx-auto flex flex-col gap-8 justify-center px-4 py-8 relative z-1">
           <div className="mt-36 flex flex-col md:flex-row space-y-16 md:gap-8">
             <div className="relative md:w-1/3 space-y-4">
@@ -248,7 +341,13 @@ const Home = () => {
                   }}
                 />
               </h3>
-              <h2>We are blessed to now have 4 incredible offices for your ultimate convenience. Our orthodontists and FreySmiles Team are excited to serve families in the Allentown, Bethlehem | Easton, Schnecksville and Lehighton communities. <Link to="/book-now">Schedule an evaluation today.</Link></h2>
+              <h2>
+                We are blessed to now have 4 incredible offices for your
+                ultimate convenience. Our orthodontists and FreySmiles Team are
+                excited to serve families in the Allentown, Bethlehem | Easton,
+                Schnecksville and Lehighton communities.{" "}
+                <Link to="/book-now">Schedule an evaluation today.</Link>
+              </h2>
             </div>
 
             <div
