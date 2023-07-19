5570c0dc846eb97c6f4e2efe6fd30f762603b382
Wanyi <wanyi.ng1975@gmail.com>
2023-07-18 22:21:27 -0400

    working bag reducers slice

--- src/store/bagSlice.js
+++ src/store/bagSlice.js
@@ -4,53 +4,36 @@ const bagSlice = createSlice({
   name: 'bag',
   initialState: {
     'line_items': [],
-    status: 'idle',
     error: null
   },
   reducers: {
     addToBag: (state, action) => {
-      const { id, name, image, price, quantity, variant } = action.payload;
-        const existingItem = state.line_items.find(item => item.id === action.payload.id)
-        if (existingItem) {
-          existingItem.quantity++
-        } else {
-          state.line_items.push({
-            id,
-            name,
-            image,
-            price,
-            quantity,
-            variant
-          })
-        }
+      const { id, quantity } = action.payload;
+      const existingItem = state.line_items.find(item => item.id === id);
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        state.line_items.push({ ...action.payload, quantity })
+      }
     },
     removeFromBag: (state, action) => {
-        const removeItem = state.line_items.filter(item => item.id !== action.payload)
-        state.line_items = removeItem
-     },
+      const removeItem = state.line_items.filter(item => item.id !== action.payload);
+      state.line_items = removeItem;
+    },
     clearBag: (state) => {
-        state.line_items = []
+        state.line_items = [];
     },
     incrementQuantity: (state, action) => {
-        const item = state.line_items.find(item => item.id === action.payload)
-        if (item) { item.product.quantity += 1 }
+      const item = state.line_items.find(item => item.id === action.payload);
+      if (item) { item.quantity += 1 }
     },
     decrementQuantity: (state, action) => {
-        const item = state.line_items.find(item => item.id === action.payload)
-        if (item && item.quantity === 1) { item.quantity = 1 }
-        else { item.quantity -= 1}
+      const item = state.line_items.find(item => item.id === action.payload);
+      if (item && item.quantity === 1) { item.quantity = 1 }
+      else { item.quantity -= 1}
     },
-    getBagTotal: (state) => {
-      let totalQuantity = 0;
-      let totalPrice = 0;
-      state.line_items.forEach(item => {
-        totalQuantity += item.quantity;
-        totalPrice += item.price * item.quantity;
-      });
-      return { totalPrice, totalQuantity };
-    }
   },
 })
 
-export const { addToBag, removeFromBag, clearBag, incrementQuantity, decrementQuantity, updateQuantity } = bagSlice.actions
+export const { addToBag, removeFromBag, clearBag, incrementQuantity, decrementQuantity } = bagSlice.actions
 export default bagSlice.reducer
