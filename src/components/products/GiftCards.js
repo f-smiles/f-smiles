78f02ec687d070c939cb713069d16c85b00a5799
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-08-16 10:00:33 -0400

    minor styling

--- src/components/products/GiftCards.js
+++ src/components/products/GiftCards.js
@@ -8,7 +8,7 @@ export default function GiftCards({props}) {
     <section className="relative max-w-screen-lg mx-auto my-24 group">
       <Link to={`${process.env.REACT_APP_SQUARE_GIFT_CARDS_URL}`} target='_blank'>
         <div className="absolute inset-0 w-full h-full bg-primary30 bg-opacity-80 text-white motion-safe:transition-[clip-path] motion-safe:duration-[2s] ease-out [clip-path:circle(50%_at_0%_0%)] lg:[clip-path:circle(35%_at_0%_0%)] group-hover:[clip-path:circle(75%_at_0%_0%)] group-hover:bg-opacity-100 flex justify-start items-start">
-          <span className='block my-[12%] mx-[6%] md:mx-[10%] md:my-[16%] lg:my-[8%] lg:mx-[3%] lg:px-12 lg:py-8 group-hover:mx-16 group-hover:my-24 transition-all group-hover:duration-[2s] ease-out md:group-hover:my-[20%] md:group-hover:mx-[16%]'>Send a Gift Card</span>
+          <span className='block my-[12%] mx-[6%] md:mx-[10%] md:my-[16%] lg:my-[8%] lg:mx-[3%] lg:px-12 lg:py-8 group-hover:mx-20 group-hover:my-32 transition-all group-hover:duration-[2s] ease-out md:group-hover:my-[20%] md:group-hover:mx-[16%]'>Send a Gift Card</span>
         </div>
         <img src="../../images/giftcards/gift_cards_mockup.jpg" alt="gift cards mockup" />
       </Link>
