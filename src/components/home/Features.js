c65e3e11c0c460def590a8fec6d3374406250341
Jo <jolee224@gmail.com>
2023-11-16 13:16:48 -0500

    dot pattern transform

--- src/components/home/Features.js
+++ src/components/home/Features.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useInView } from "framer-motion";
 import DotPattern from "../svg/DotPattern";
 import { motion, useScroll, useTransform } from "framer-motion";
+
 function Hero() {
   const charStyle = {
     position: "absolute",
@@ -31,7 +32,7 @@ function Hero() {
     id="hero-section"
     className=""
   >
-  
+
     <div className="relative px-8 isolate  lg:px-8">
       <div
         id="gradients"
@@ -40,13 +41,27 @@ function Hero() {
       >
       </div>
       <div className="grid max-w-screen-xl grid-cols-1 py-32 mx-auto sm:py-48 lg:py-56 place-items-center lg:grid-cols-2">
+
 <div className="relative mx-auto mt-32 lg:mt-0">
 <div className="flex items-center justify-center flex-wrap">
-  <div className="absolute w-full  -top-1/2 scale-110 -z-10">
+<div className="absolute w-full -top-1/2 scale-110 -z-10">
+  <svg width="500" height="500" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ transform: 'rotate(180deg)' }}>
+    <g>
+      <path d="M0 0H100C155.228 0 200 44.7715 200 100V200H100C44.7715 200 0 155.228 0 100V0Z" fill="url(#paint0_linear_221_10)" />
+    </g>
+    <defs>
+      <linearGradient id="paint0_linear_221_10" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#e1e5fa" />
+        <stop offset="1" stop-color="#fceeff" />
+      </linearGradient>
+      <clipPath id="clip0_221_10">
+        <rect width="200" height="200" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
 
-  <svg width="500" height="500" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_221_10)"> <path d="M0 0H100C155.228 0 200 44.7715 200 100V200H100C44.7715 200 0 155.228 0 100V0Z" fill="url(#paint0_linear_221_10)"/> </g> <defs> <linearGradient id="paint0_linear_221_10" x1="100" y1="0" x2="100" y2="200" gradientUnits="userSpaceOnUse"> <stop stop-color="#e1e5fa"/> <stop offset="1" stop-color="#fceeff"/> </linearGradient> <clipPath id="clip0_221_10"> <rect width="200" height="200" fill="white"/> </clipPath> </defs> </svg>
-    <DotPattern />
-  </div>
+</div>
+
 <div className="font-HelveticaNowVar font-extralight text-white text-4xl" style={{ whiteSpace: 'nowrap', textAlign: 'center', lineHeight: '1' }}>
 <span style={{ fontSize: '4rem' }}>Because <div className="italic" style={{ display: 'inline' }}>Every</div> Smile</span><br />
 <span style={{ fontSize: '4rem' }}>Is Unique</span>
@@ -83,14 +98,7 @@ function Hero() {
         className="absolute inset-x-0 top-[calc(100%-23rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]"
         aria-hidden="true"
       >
-        
-        {/* <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 452% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        /> */}
+   
       </div>
    
     </div>
@@ -130,31 +138,62 @@ export default function Features() {
   
   return (
     <>
+      
     <div class="snap-y snap-mandatory bg-gradient-to-r from-stone-100 via-stone-100 to-purple-200 h-screen overflow-scroll">
-    <div class="snap-start  w-screen h-screen flex items-center justify-center text-5xl">
+ 
+    <div class="snap-start flex items-center justify-center text-5xl">
     <Section >
+    <svg width="100" height="100" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M200 150C200 94.7715 155.228 50 100 50C44.7715 50 0 94.7715 0 150H200Z" fill="url(#paint0_linear_105_460)"/> <defs> <linearGradient id="paint0_linear_105_460" x1="27.5" y1="59.5" x2="69.9415" y2="168.136" gradientUnits="userSpaceOnUse"> <stop stop-color="#FFD9A0"/> <stop offset="1" stop-color="#FFF5F1"/> </linearGradient> </defs> </svg>
+    <img
+  src="/images/logo_icon.png"
+  alt="logo"
+  className="w-20 h-20 "
+/>
+
           <Hero />
         </Section>
         </div>
-    <div class="snap-start bg-[#EFEEF4] w-screen h-screen flex items-center justify-center text-5xl">
+    <div class="snap-start bg-[#EFEEF4]  text-2xl">
     <Section color="bg-D1CCe1"  >
-<h3 className="text-black absolute top-0 left-1/2 transform -translate-x-1/2 mt-10 text-6xl font-gilroy-light w-1/2 text-center">Invisalign</h3>
-  <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto lg:flex-row">
- 
-    <div className="bg-white gap-6 space-y-4 text-gray-700 md:w-1/3">
-      <p className="text-xl pb-4">
+
+<div className=" flex max-w-screen-xl mx-auto">
+
+  <div className="flex flex-col justify-start w-full lg:w-1/2 ">
+
+  <DotPattern style={{
+  WebkitTransform: 'scaleX(-1)',
+  transform: 'scaleX(-1)'
+}} />
+  
+    <p className="text-xl pb-4">
         As part of the top 1% of Invisalign providers in the US, we have the experience to deliver the smile you deserve.
       </p>
-      <Link className="mb-10 inline-block px-6 py-4 transition-colors duration-300 ease-linear border rounded-full border-[#e67fb4] hover:bg-gray-800 hover:border-0 hover:text-white" to="/invisalign">How Invisalign Works</Link>
+    <div className="space-y-4 text-gray-700">
+   
+
     </div>
-    <figure className="w-1/2">
-      <img
-        className="object-contain aspect-video"
-        src="../images/aligner.png"
-        alt="invisalign"
-      />
-    </figure>
   </div>
+
+
+<div className="w-full ">
+  <figure>
+    <img
+      className="object-contain aspect-video"
+      src="../images/aligner.png"
+      alt="invisalign"
+    />
+  </figure>
+  <Link
+    className="text-black mb-10 px-6 py-2 transition-colors duration-300 ease-linear border rounded-full border-[#e67fb4] hover:bg-gray-800 hover:border-0 hover:text-white"
+    to="/invisalign"
+  >
+    How Invisalign Works
+  </Link>
+</div>
+
+</div>
+
+
 
 </Section>
 </div>
